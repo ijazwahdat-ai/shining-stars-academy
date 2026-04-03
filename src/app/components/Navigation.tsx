97fbb71d48474c512c20bceb4a323b9d59e51e7f
Wahdat <ijazulhaqwahdat01@gmail.com>
2026-04-03 18:14:35 +0430

    توضیح کوتاه درباره تغییری که دادید

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -26,7 +26,7 @@ export default function Navigation() {
             <div className="w-10 h-10 bg-blue-600 rounded-lg flex items-center justify-center">
               <Star className="w-6 h-6 text-white" />
             </div>
-            <span className="text-xl font-bold text-white">Shining Stars</span>
+            <span className="text-xl font-bold text-white">Shining Stars Online Languages & Computer Science Academy</span>
           </Link>
 
           <div className="hidden md:flex space-x-8">
